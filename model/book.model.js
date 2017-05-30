b9ebe0e9956528343b8b4bd26572d10372bb65b5
Rishanthakumar Rasarathinam <rishanthakumarr@zone24x7.com>
2017-05-30 17:33:33 +0530

    Add Sample object model

--- model/book.model.js
+++ model/book.model.js
@@ -1,1 +1,28 @@
+'use strict';
 
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+    title: String,
+    published: {
+        type: Date,
+        default: Date.now
+    },
+    keywords: Array,
+    publishedStatus: Boolean,
+    author: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+
+    // Embedded sub-document
+    detail: {
+        modelNumber: Number,
+        hardCover: Boolean,
+        reviews: Number,
+        rank: Number
+    }
+});
+
+module.exports = mongoose.model('Book', BookSchema);
